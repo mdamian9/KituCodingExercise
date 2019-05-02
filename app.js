eb89f9a6379a24547c1ad8bb72a02454eb222c6f
mdamian9 <m.damian9@hotmail.com>
2019-05-02 14:19:55 -0700

    Installed axios and body-parser, tested routes with Postman

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 const PORT = 3000;
 
@@ -7,6 +8,9 @@ const indexRouter = require('./routes/index');
 
 // Log all requests to the console
 app.use(logger('dev'));
+// Use body-parser to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Using express.Router just 'cause
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,67 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
+// We will store our users in memory
+let usersArr = [];
+
 router.get('/users', (req, res) => {
-    res.send('users get endpoint');
+    // Request to 3rd party API, get 10 results
+    axios.get('https://randomuser.me/api/?results=10')
+        .then((response) => {
+            // For each loop to iterate over data array
+            response.data.results.forEach(user => {
+                // Create newUser object from response data
+                const newUser = {
+                    gender: user.gender,
+                    firstname: user.name.first,
+                    city: user.location.city,
+                    email: user.email,
+                    cell: user.cell
+                };
+                // Push new user into users array
+                usersArr.push(newUser);
+            });
+            res.statusCode = 200;
+            res.send(usersArr);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 router.post('/users', (req, res) => {
-    res.send('users post endpoint');
+    // Create newUser object from POST request data
+    const newUser = {
+        gender: req.body.gender,
+        firstname: req.body.firstname,
+        city: req.body.city,
+        email: req.body.email,
+        cell: req.body.cell
+    };
+    // Push new user into users array
+    usersArr.push(newUser);
+    res.statusCode = 201;
+    res.send({ message: 'User successfully created!' });
 });
 
 router.get('/users/firstname/:firstname', (req, res) => {
-    res.send('users/firstname/:firstname');
+    /*
+        Filter through users stored in memory. This will return an array with one element,
+        so we'll set targetUser equal to the first element in the array.
+    */
+    const targetUser = usersArr.filter((user) => {
+        return user.firstname === req.params.firstname;
+    })[0];
+
+    // If user exists, everything is ok. If user DNE, set status code to 404, send back error
+    if (targetUser) {
+        res.statusCode = 200;
+        res.json(targetUser);
+    } else {
+        res.statusCode = 404;
+        res.json({ message: 'User not found!' });
+    };
 });
 
 module.exports = router;
