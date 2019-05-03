c386d819170343948dd4a3269f3c36876f820151
mdamian9 <m.damian9@hotmail.com>
2019-05-03 12:32:10 -0700

    Modified /users route to use 10 async requests, added /users-2 route which only uses 1 async request

--- routes/index.js
+++ routes/index.js
@@ -6,41 +6,63 @@ const router = express.Router();
 let usersArr = [];
 
 router.get('/users', (req, res) => {
-    // Request to 3rd party API, get 10 results
-    axios.get('https://randomuser.me/api/?results=10')
-        .then((response) => {
-            // For each loop to iterate over data array
-            response.data.results.forEach(user => {
-                // Create newUser object from response data
-                const newUser = {
-                    gender: user.gender,
-                    firstname: user.name.first,
-                    city: user.location.city,
-                    email: user.email,
-                    cell: user.cell
-                };
-                // Push new user into users array
-                usersArr.push(newUser);
-            });
-            res.statusCode = 200;
-            res.send(usersArr);
-        })
-        .catch((err) => {
-            console.log(err);
+    // Array to hold axios promises (10 asynchronous requests), which we will resolve with Promise.all
+    let promises = [];
+    // Run loop 10 times to make 10 requests, then push promises into promises array
+    for (let i = 1; i <= 10; i++) {
+        promises.push(axios.get('https://randomuser.me/api/'));
+    };
+    // Resolve promises and use our data
+    Promise.all(promises).then(values => {
+        // For each loop to iterate over data array
+        values.forEach(response => {
+            // Push new user object into users array
+            usersArr.push({
+                gender: response.data.results[0].gender,
+                firstname: response.data.results[0].name.first,
+                city: response.data.results[0].location.city,
+                email: response.data.results[0].email,
+                cell: response.data.results[0].cell
+            })
         });
+        res.statusCode = 200;
+        res.send(usersArr);
+    }).catch(err => {
+        console.log(err.message)
+    });
+});
+
+// Added this extra route, which only uses 1 async request rather than 10 requests to obtain 10 new users
+router.get('/users-2', (req, res) => {
+    // Request to 3rd party API, get 10 results
+    axios.get('https://randomuser.me/api/?results=10').then(response => {
+        // For each loop to iterate over data array
+        response.data.results.forEach(user => {
+            // Push new user object into users array
+            usersArr.push({
+                gender: user.gender,
+                firstname: user.name.first,
+                city: user.location.city,
+                email: user.email,
+                cell: user.cell
+            });
+        });
+        res.statusCode = 200;
+        res.send(usersArr);
+    }).catch((err) => {
+        console.log(err);
+    });
 });
 
 router.post('/users', (req, res) => {
-    // Create newUser object from POST request data
-    const newUser = {
+    // Create newUser object from POST request data and push into users array
+    usersArr.push({
         gender: req.body.gender,
         firstname: req.body.firstname,
         city: req.body.city,
         email: req.body.email,
         cell: req.body.cell
-    };
-    // Push new user into users array
-    usersArr.push(newUser);
+    });
     res.statusCode = 201;
     res.json({ message: 'User successfully created!' });
 });
